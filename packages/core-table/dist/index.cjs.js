298a2a66fc03ac264a456e24f94ab5168ff87a17
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2025-01-28 03:35:48 +0900

    feat: #3 core-table 변경사항docs에 실시간 반영

---
+++ packages/core-table/dist/index.cjs.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const Primary = () => {
+    const button = document.createElement('button');
+    button.innerText = 'Click Me!!!!~!~';
+    button.onclick = () => alert('Button clicked!');
+    return button;
+};
+
+exports.Primary = Primary;
+//# sourceMappingURL=index.cjs.js.map
