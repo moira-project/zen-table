f362cd44c670c7ce987eee5a94e9e97d3584ce21
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2025-02-02 20:11:35 +0900

    feat: 구조 types def suffix 추가 (refs #5)

--- packages/core-table/src/types.ts
+++ packages/core-table/src/types.ts
@@ -1,23 +1,23 @@
-export type Row = unknown | object | any[];
+export type RowDef = unknown | object | any[];
 
-export interface Table<TData extends Row> {
-  columns: Column<TData>[];
+export interface TableDef<TData extends RowDef> {
+  columns: ColumnDef<TData>[];
   data: TData[];
 }
 
-export interface ColumnKey<TData extends Row> {
+export interface ColumnKeyDef<TData extends RowDef> {
   key: (string & {}) | keyof TData; // string&{} 은 리터럴 타입을 유지하기 위함.
 }
 
-export interface ColumnHeader {
+export interface ColumnHeaderDef {
   header: string;
 }
 
-export interface ColumnStyle {
+export interface ColumnStyleDef {
   width?: number;
 }
 
-export interface Column<TData extends Row, TValue = unknown>
-  extends ColumnKey<TData>,
-    ColumnHeader,
-    ColumnStyle {}
+export interface ColumnDef<TData extends RowDef, TValue = unknown>
+  extends ColumnKeyDef<TData>,
+    ColumnHeaderDef,
+    ColumnStyleDef {}
